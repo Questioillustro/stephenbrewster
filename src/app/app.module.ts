f4871dbdaeafdeabb8cf5bef580b7d7efa7ca341
Stephen <questioillustro@gmail.com>
2021-03-18 19:22:43 -0400

    Basic outline, theme, icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material';
+import { FooterComponent } from './footer/footer.component';
+import {FlexModule} from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexModule
   ],
   providers: [],
   bootstrap: [AppComponent]
